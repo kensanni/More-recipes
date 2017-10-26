a60e1ee9b01a00a0f2e62f0b2ceaf3dec948e011
kensanni <kensanni@gmail.com>
2017-10-26 15:54:05 +0100

    Require routes into my app [#152234075]

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import routes from '../server/routes';
 
 // Set up the express app
 const app = express();
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
+routes(app)
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
