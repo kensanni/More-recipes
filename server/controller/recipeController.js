8e5244946b935203b151380e779e78070247e26b
kensanni <kensanni@gmail.com>
2017-10-27 09:55:32 +0100

    Create API endpoint to post review for recipes [#152119569]

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -78,4 +78,5 @@ class handleRecipeMethod {
     });
   }
 }
+
 export default handleRecipeMethod;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 import handleRecipeMethod from '../controller/recipeController';
+import reviewHandler from '../controller/reviewController';
 
 export default (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -7,5 +8,6 @@ export default (app) => {
   app.post('/api/v1/recipes', handleRecipeMethod.addRecipe);
   app.get('/api/v1/recipes', handleRecipeMethod.getAllRecipe);
   app.delete('/api/v1/recipes/:recipeDeleteId', handleRecipeMethod.deleteRecipe);
+  app.post('/api/v1/recipes/:recipeId/reviews', reviewHandler.addReview);
 };
 
