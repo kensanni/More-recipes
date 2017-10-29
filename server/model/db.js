7361bdc97a7792952f8b80fbd6c27dd7f2360dd5
kensanni <kensanni@gmail.com>
2017-10-29 02:50:40 +0100

    Modify dummy data to include review [#152305804]

--- server/model/db.js
+++ server/model/db.js
@@ -9,6 +9,7 @@ db.recipes = [
     description: 'simple dish',
     upvotes: 30,
     downvotes: 4,
+    reviews: []
   },
   {
     id: 2,
@@ -18,6 +19,47 @@ db.recipes = [
     description: 'simple dish',
     upvotes: 1,
     downvotes: 40,
+    reviews: []
+  },
+  {
+    id: 3,
+    userId: 3,
+    name: 'Jollof rice',
+    mealType: 'Lunch',
+    description: 'simple dish',
+    upvotes: 1,
+    downvotes: 40,
+    reviews: []
+  },
+  {
+    id: 4,
+    userId: 3,
+    name: 'Jollof rice',
+    mealType: 'Lunch',
+    description: 'simple dish',
+    upvotes: 1,
+    downvotes: 40,
+    reviews: []
+  },
+  {
+    id: 5,
+    userId: 3,
+    name: 'Jollof rice',
+    mealType: 'Lunch',
+    description: 'simple dish',
+    upvotes: 1,
+    downvotes: 40,
+    reviews: []
+  },
+  {
+    id: 6,
+    userId: 3,
+    name: 'Jollof rice',
+    mealType: 'Lunch',
+    description: 'simple dish',
+    upvotes: 1,
+    downvotes: 40,
+    reviews: []
   },
 ];
 
