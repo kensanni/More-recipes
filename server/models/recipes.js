ebe8bbc0f013249f5ae7e6421664fc0c96d1740e
kensanni <kensanni@gmail.com>
2017-10-31 21:02:32 +0100

    Create recipes model [#152413834]

---
+++ server/models/recipes.js
@@ -0,0 +1,35 @@
+export default (sequelize, DataTypes) => {
+  const Recipes = sequelize.define('Recipes', {
+    recipeName: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please input your recipe name'
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please Input a description for your recipes'
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please input a image for your recipes'
+      }
+    },
+    indegrient: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please input the required indegrient for your recipes'
+      }
+    }
+  });
+  Recipes.associate = (models) => {
+  };
+  return Recipes;
+};
