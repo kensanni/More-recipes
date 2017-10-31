2a164baac75a1246acfc139b817015f39d5727d8
kensanni <kensanni@gmail.com>
2017-10-31 21:24:54 +0100

    Create association between user and recipes [#152413834]

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,10 @@ export default (sequelize, DataTypes) => {
     image: DataTypes.STRING
   });
   User.associate = (models) => {
+    User.hasMany(models.Recipes, {
+      foreignKey: 'userId',
+      as: 'recipes',
+    });
   };
   return User;
 };
