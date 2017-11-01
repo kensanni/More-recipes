518963a7a974010ba9833834a43bc72f67aa83bf
kensanni <kensanni@gmail.com>
2017-11-01 03:55:03 +0100

    Create amethod to add recipes [#152449126]

---
+++ server/controller/recipesController.js
@@ -0,0 +1,54 @@
+import model from '../models';
+
+const { Recipes } = model;
+
+/**
+ * @class recipesController
+*/
+class handleRecipesMethod {
+  /**
+   * @description Adds a recipe
+   * @param {*} req
+   * @param {*} res
+   * @returns  {JSON} Returns a JSON object
+   */
+  static addRecipes(req, res) {
+    const {
+      recipeName, description, indegrient, image
+    } = req.body;
+    if (!recipeName) {
+      return res.status(400).send({
+        message: 'Please input the name of your recipe'
+      });
+    }
+    if (!description) {
+      return res.status(400).send({
+        message: 'Please input a description for your recipe'
+      });
+    }
+    if (!indegrient) {
+      return res.status(400).send({
+        message: 'Please input a description for your recipe'
+      });
+    }
+    if (!image) {
+      return res.status(400).send({
+        message: 'Please upload an image of your recipes'
+      });
+    }
+    return Recipes
+      .create({
+        recipeName,
+        description,
+        indegrient,
+        image
+      })
+      .then(recipe => res.status(201).send({
+        success: true,
+        messge: 'Recipe successfully created',
+        recipeName: recipe.recipeName,
+        id: recipe.id,
+      }));
+  }
+}
+export default handleRecipesMethod;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 import handleUserMethod from '../controller/userController';
+import handleRecipeMethod from '../controller/recipesController';
 import reviewHandler from '../controller/reviewController';
 
 export default (app) => {
@@ -8,7 +9,7 @@ export default (app) => {
 
   app.post('/api/v1/users/signup', handleUserMethod.userSignUp);
   app.post('/api/v1/users/signin', handleUserMethod.userSignIn);
-  // app.post('/api/v1/recipes', handleRecipeMethod.addRecipe);
+  app.post('/api/v1/recipes', handleRecipeMethod.addRecipes);
   // app.get('/api/v1/recipes', handleRecipeMethod.getAllRecipe);
   // app.put('/api/v1/recipes/:recipeUpdateId', handleRecipeMethod.modifyRecipe);
   // app.delete('/api/v1/recipes/:recipeDeleteId', handleRecipeMethod.deleteRecipe);
