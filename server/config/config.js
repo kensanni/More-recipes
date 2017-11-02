cc21ac320d2ca407ff000b6d3276dd625e085af5
kensanni <kensanni@gmail.com>
2017-11-02 19:41:22 +0100

    Fix dotenv error [#152413834]

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     host: '127.0.0.1',
     dialect: 'postgres'
   },
