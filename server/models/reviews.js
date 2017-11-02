182c91e4f627a821aa18c86ba6f4471c847886bc
kensanni <kensanni@gmail.com>
2017-11-02 06:09:40 +0100

    Create API endpoint to delete a recipe [#152498569]

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -28,7 +28,7 @@ export default (sequelize, DataTypes) => {
       foreignKey: 'recipeId',
       onDelete: 'CASCADE'
     });
-    Reviews.belongsTo(models.Recipes, {
+    Reviews.belongsTo(models.Users, {
       foreignKey: 'recipeId',
       onDelete: 'CASCADE'
     });
