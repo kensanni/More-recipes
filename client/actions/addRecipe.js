c66ae1b87d31363ceb529ec5d1f674a8847a2d8b
Sanni Kehinde <kensanni@gmail.com>
2018-02-24 00:16:23 +0100

    create actions for adding recipe
    [#155315453]

---
+++ client/actions/addRecipe.js
@@ -0,0 +1,20 @@
+export const ADD_RECIPE_REQUEST = 'ADD_RECIPE_REQUEST';
+export const ADD_RECIPE_SUCCESSFUL = 'ADD_RECIPE_SUCCESSFUL';
+export const ADD_RECIPE_ERROR = 'ADD_RECIPE_ERROR';
+
+export const addRecipeRequest = recipeData => ({
+  type: ADD_RECIPE_REQUEST,
+  recipeData,
+  isAdded: false
+});
+export const addRecipeSuccess = (responseMessage, newRecipeData) => ({
+  type: ADD_RECIPE_SUCCESSFUL,
+  responseMessage,
+  newRecipeData,
+  isAdded: true
+});
+export const addRecipeError = errorMessage => ({
+  type: ADD_RECIPE_ERROR,
+  errorMessage,
+  isAdded: false
+});
