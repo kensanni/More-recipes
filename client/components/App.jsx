a43e78aead8a8fd450d1858bc86406b668a88b8a
Sanni Kehinde <kensanni@gmail.com>
2018-02-24 11:44:16 +0100

    creates a method to save userID to store

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Router, Route, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import LandingPage from './recipes/LandingPage';
-import SignUp from '../components/signupPage/connectedComponent/SignUp';
-import SignIn from './signinPage/connectedComponent/SignIn';
-import UserRecipes from './recipes/userRecipes/UserRecipes';
 import RecipeGrid from './recipes/RecipeGrid';
+import UserRecipes from './recipes/userRecipes/UserRecipes';
+import SignUp from './signupPage/connectedComponent/SignUp';
+import SignIn from './signinPage/connectedComponent/SignIn';
+import { signInFromLocalStorage } from '../actionController/signin';
 
-const App = () => ((
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/profile" component={UserRecipes} />
-        <Route path="/recipes" component={RecipeGrid} />
-      </Switch>
-    </div>
-  </Router>
-));
+const history = createBrowserHistory();
 
-export default App;
+/**
+ * @class App
+ */
+class App extends Component {
+  /**
+   * @description save the userID to store when component mounts
+   * @param {*} props
+   * @return {object} object
+   */
+  componentWillMount() {
+    this.props.signInFromLocalStorage();
+  }
+
+  /**
+   * @description display the app
+   * @returns {*} app
+   */
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/recipes" component={RecipeGrid} />
+            <Route path="/profile" component={UserRecipes} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+App.propTypes = {
+  signInFromLocalStorage: PropTypes.func.isRequired
+};
+
+export default connect(null, { signInFromLocalStorage })(App);
