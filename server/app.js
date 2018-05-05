bead68b7d0393f10ae3468b0a8f06344fcc511cd
kensanni <kensanni@gmail.com>
2018-05-06 00:00:06 +0100

    chore(devOps): awazon web services deployment
    use babel to build file to a new destination
    setup server in a new file

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import webpack from 'webpack';
 import path from 'path';
+import http from 'http';
 import validator from 'express-validator';
 import swaggerUi from 'swagger-ui-express';
 import config from '../webpack.config';
@@ -20,6 +21,8 @@ let compiler;
 
 
 const app = express();
+const port = parseInt(process.env.PORT, 10) || 8000;
+const server = http.createServer(app);
 
 if (process.env.NODE_ENV === 'production') {
   compiler = webpack(prodConfig);
@@ -52,6 +55,12 @@ app.all('*', (req, res) => {
   res.status(404).send({
     message: 'Route does not exist'
   });
+});
+
+app.set('port', port);
+
+server.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
 
 export default app;
