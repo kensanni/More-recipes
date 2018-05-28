24d473a2658013be0143b7e3afc1fbf759109444
kensanni <kensanni@gmail.com>
2018-05-28 16:37:10 +0100

    chore(web_app): split web app

--- server/app.js
+++ server/app.js
@@ -1,40 +1,20 @@
+import 'babel-core/register';
+import 'babel-polyfill';
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import webpack from 'webpack';
 import path from 'path';
 import validator from 'express-validator';
 import swaggerUi from 'swagger-ui-express';
-import config from '../webpack.config';
-import prodConfig from '../webpack.config.prod';
 import routes from '../server/routes';
 import apiDocs from './api-docs.json';
 
-require('babel-core/register');
-require('babel-polyfill');
 require('dotenv').config();
-
-const HMR = require('webpack-hot-middleware');
-
-let compiler;
-
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  compiler = webpack(prodConfig);
-} else {
-  compiler = webpack(config);
-  app.use(HMR(compiler));
-}
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
 
 app.use(logger('dev'));
 
